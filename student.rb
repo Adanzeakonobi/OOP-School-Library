85a5b90a673e83f4c5539997b12a77b1d99c90e2
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-07-18 13:50:54 -0600

    Correct linter errors

--- student.rb
+++ student.rb
@@ -1,15 +1,14 @@
 require_relative 'person'
 
-class Student
-    class Student < Person
-    attr_accessor :classroom
+class Student < Person
+  attr_accessor :classroom
 
-    def initialize(classroom, age, name = 'Unknown', parent_permission: true)
-        super(age, name, parent_permission)
-      @classroom = classroom
-    end
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
 
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,15 +1,14 @@
 require_relative 'person'
 
-class Teacher
-    class Teacher < Person
-    attr_accessor :specialization
+class Teacher < Person
+  attr_accessor :specialization
 
-    def initialize(specialization, age, name = 'Unknown', parent_permission: true)
-        super(age, name, parent_permission)
-      @@specialization = specialization
-    end
+  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
 
-    def can_use_services?
-        true
-      end
+  def can_use_services?
+    true
+  end
 end
