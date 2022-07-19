33447db37d7989eda9bf59a8d49c39b22511cca7
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-07-19 13:28:38 -0600

    Turn the person class to Nameable

--- person.rb
+++ person.rb
@@ -1,4 +1,6 @@
-class Person
+require_relative 'nameable'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age, :parent_permission
 
@@ -11,6 +13,10 @@ class Person
 
   def can_use_services?
     of_age? || @parent_permission ? true : false
+  end
+
+  def correct_name
+    @name
   end
 
   private
