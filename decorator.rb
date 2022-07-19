d8dbe73f9a6990c9dadab3e1dc9b332c8432de15
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-07-19 13:50:03 -0600

    Prepare CapitalizeDecorator and TrimmerDecorator

---
+++ decorator.rb
@@ -0,0 +1,26 @@
+require_relative 'nameable'
+
+class Decorator < Nameable
+    attr_accessor :nameable
+
+    def initialize(nameable)
+        super()
+        @nameable = nameable
+      end
+    
+      def correct_name
+        @nameable.correct_name
+      end
+end
+
+class CapitalizeDecorator < Decorator
+    def correct_name
+        @nameable.correct_name.capitalise
+      end
+end 
+
+class TrimmerDecorator < Decorator
+    def correct_name
+        @nameable.correct_name.slice(0, 10)
+      end
+end 
