2e1a67580797a2e6359bf308e9147297e6ece599
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-07-21 15:45:15 -0600

    create method for create teacher

--- app.rb
+++ app.rb
@@ -102,5 +102,22 @@ class App
             sleep 0.75
           end
 
+          def create_teacher
+            print 'Age: '
+            age = gets.chomp.to_i
+        
+            print 'Name: '
+            name = gets.chomp
+        
+            print 'Specialization: '
+            specialization = gets.chomp
+        
+            teacher = Teacher.new(specialization, age, name)
+            @people << teacher
+        
+            puts 'Teacher created successfully'
+            sleep 0.75
+          end
+
 
 end
