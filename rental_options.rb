719ae79f963e58d9e60f1f1e7ac2f88fb11eb944
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-07-25 13:58:39 -0600

    Created Module for rentals

---
+++ rental_options.rb
@@ -0,0 +1,44 @@
+require './book_options'
+require './people_options'
+
+class RentalOptions
+  attr_reader :books, :people
+
+  def initialize
+    @rentals = []
+  end
+
+  def create_rental
+    puts 'Select a book from the following list by number'
+    @books.each_with_index { |book, index| puts "#{index}) Title: #{book.title}, Author: #{book.author}" }
+
+    book_id = gets.chomp.to_i
+
+    puts 'Select a person from the following list by number (not id)'
+    @people.each_with_index do |person, index|
+      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+
+    person_id = gets.chomp.to_i
+
+    print 'Date: '
+    date = gets.chomp.to_s
+
+    rental = Rental.new(date, @books[book_id], @people[person_id])
+    @rentals << rental
+
+    puts 'Rental created successfully'
+    sleep 0.75
+  end
+
+  def list_rentals_by_person_id
+    print 'ID of person: '
+    id = gets.chomp.to_i
+
+    puts 'Rentals:'
+    @rentals.each do |rental|
+      puts "Date: #{rental.date}, Book '#{rental.book.title}' by #{rental.book.author}" if rental.person.id == id
+    end
+    sleep 0.75
+  end
+end
