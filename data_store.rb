0e363391748a18123b68344c899b312d44943536
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-07-26 11:46:40 -0600

    Added function for save person

--- data_store.rb
+++ data_store.rb
@@ -10,3 +10,16 @@ module DataStore
     def load_data(filename)
         JSON.parse(file.read(filename))
     end
+
+    def save_person
+        data = []
+        @people.each do |person|
+            if person.instance_of?(Teacher)
+                data << ({key: 'teacher', id: person.id, age: person.age, name: person.name, specialization: person.specialization})
+            else
+                data << ({key: 'teacher', id: person.id, age: person.age, name: person.name, parent_permission: person.parent_permission, classroom: person.classroom})
+            end
+            save_data('person.json', data)
+        end
+    end
+end
